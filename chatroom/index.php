c11e47fba7b75bfb45fac282f889ace2a87860be
kevin <kevin@192.168.2.10>
2017-08-22 20:55:04 -0500

    Aug 22

--- chatroom/index.php
+++ chatroom/index.php
@@ -1,32 +1,121 @@
 <!DOCTYPE html>
 <html>
 <head>
-<!-- Using the jquery CDN -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script> 
-$(document).ready(function(){
-    $("#animation").click(function(){
-        $("#figure").animate({left: '250px'});
-        //The CSS property object are passed
-    });
-});
-</script>
-<style>
-.circle:before {
-  content: ' \25CF';
-  font-size: 200px;    
-}
-.circle{
-        position:absolute;
-    }    
-</style>
+<title>Chat - Customer Module</title>
+<link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 <body>
 
-<button id="animation">Start Animation</button>
-<p>This example shows a basic animation using the CSS properties object parameter, keep in mind that all HTML element are static by default we need to set the element's CSS position property of the element to relative, fixed, or absolute!</p>
+<?php
+session_start();
+
+if(!isset($_SESSION['name'])){
+    loginForm();
+}
+else{
+?>
+    <div id="wrapper">
+        <div id="menu">
+            <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+            <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+            <div style="clear:both"></div>
+        </div>    
+        <div id="chatbox"></div>
+         
+        <form name="message" action="">
+            <input name="usermsg" type="text" id="usermsg" size="63" />
+            <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+        </form>
+    </div>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+    <script type="text/javascript">
+    // jQuery Document
+    $(document).ready(function(){
+    	//If user wants to end session
+    	$("#exit").click(function(){
+    		var exit = confirm("Are you sure you want to end the session?");
+    		if(exit==true){window.location = 'index.php?logout=true';}		
+    	});
+    });
+    </script>
+<?php
+}
+
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+
+if(isset($_GET['logout'])){
     
-    <div id="figure" class="circle"></div>
+    //Simple exit message
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+    fclose($fp);
+    
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
+
+?>
+
+
+<?php
+
+function loginForm(){
+    echo '
+    <div id="loginform">
+    <form action="index.php" method="post">
+        <p>Please enter your name to continue:</p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+        <input type="submit" name="enter" id="enter" value="Enter" />
+    </form>
+    </div>
+    ';
+}
+?>
+
+<script>
+	//If user submits the form
+	$("#submitmsg").click(function(){	
+		var clientmsg = $("#usermsg").val();
+		$.post("post.php", {text: clientmsg});				
+		$("#usermsg").attr("value", "");
+		return false;
+	});	
+</script>
+
+
+<script>
+//Load the file containing the chat log
+	//Load the file containing the chat log
+	function loadLog(){		
+		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+		$.ajax({
+			url: "log.html",
+			cache: false,
+			success: function(html){		
+				$("#chatbox").html(html); //Insert chat log into the #chatbox div	
+				
+				//Auto-scroll			
+				var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+				if(newscrollHeight > oldscrollHeight){
+					$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+				}				
+		  	},
+		});
+	}
+//---------------------------------------------------------------------
+</script>
+
+<script type="text/javascript">
+setInterval (loadLog, 2500);	//Reload file every 2500 ms or x ms if you wish to change 
+</script>
 
 </body>
 </html>
